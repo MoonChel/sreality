e488504c968ebb99a3efd172260a57643929d99e
MoonChel <vlado.chernenko@gmail.com>
2021-10-31 19:50:20 +0100

    add Prices in other countries

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,14 +1,55 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>Prices in Czech Republic and other countries</h1>
+    <ul>
+      <li>
+        <a
+          href="https://www2.deloitte.com/content/dam/Deloitte/cz/Documents/real-estate/CZ_DELOITTE_DEVELOP_INDEX_21_06.pdf"
+        >
+          Prague, capital of Czech Republic ~ 4,888.43 €/m2
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.idnes.cz/brno/zpravy/byty-bydleni-brno-ceny-nemovitost.A210607_611790_brno-zpravy_mos1"
+        >
+          Brno, not even capital of Czech Republic ~ 4,225.13 €/m2
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.idealista.com/sala-de-prensa/informes-precio-vivienda/venta/madrid-comunidad/madrid-provincia/madrid/)"
+        >
+          Madrid, capital of Spain ~ 3.710 €/m2
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.parisestimation.com/informations_prix_appartement_paris"
+        >
+          Paris, capital of France ~ 6850 € et 12 630 €/m²
+        </a>
+      </li>
+      <li>
+        <a href="https://www.wohnungsboerse.net/immobilienpreise-Berlin/2825">
+          Berlin, capital of Germany ~ 5,841.63 €/m²
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.vienna.at/preise-fuer-wohnungen-in-wien-steigen-ausser-in-einem-bezirk/7140084"
+        >
+          Vienna, capital of Austria ~ 4,263 € to 4,499 €/m²
+        </a>
+      </li>
+    </ul>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: "About us",
+  },
+};
 </script>
